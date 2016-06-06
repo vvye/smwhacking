6b683f2e60b256b15da7b446c50de2634b2002ed
vvye <ekaiser.hellwege@gmail.com>
2016-06-06 23:15:52 +0200

    added content

--- index.php
+++ index.php
@@ -1,1 +1,41 @@
-Hello World!
+<?php
+
+	/*
+		smwhacking.de
+		WYE, 2016
+	*/
+
+?>
+<!doctype html>
+<html>
+
+	<head>
+		<title>smwhacking.de - die deutschsprachige Seite übers SMW-Hacken</title>
+	</head>
+
+	<body>
+
+		<header>
+			<h1>smwhacking.de</h1>
+			<p>die deutschsprachige Seite übers SMW-Hacken</p>
+			<nav>
+				<ul>
+					<li>Start</li>
+					<li>Was ist SMW-Hacken?</li>
+					<li>Forum</li>
+					<li>Chat</li>
+					<li>Uploader</li>
+				</ul>
+			</nav>
+			<div>
+				Einloggen
+			</div>
+		</header>
+
+		<div>
+			Willkommen!
+		</div>
+
+	</body>
+
+</html>
