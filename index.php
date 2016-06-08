4bfab336716c70c0e70629070ee6c09050929362
vvye <ekaiser.hellwege@gmail.com>
2016-06-08 23:05:42 +0200

    first design draft

--- index.php
+++ index.php
@@ -8,32 +8,65 @@
 ?>
 <!doctype html>
 <html>
-
 	<head>
+		<meta charset="utf-8" />
+
 		<title>smwhacking.de - die deutschsprachige Seite übers SMW-Hacken</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic" rel="stylesheet"
+		      type="text/css" />
+		<link rel="stylesheet" type="text/css" href="css/normalize.min.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+
 	</head>
 
 	<body>
 
 		<header>
-			<h1>smwhacking.de</h1>
-			<p>die deutschsprachige Seite übers SMW-Hacken</p>
+			<div class="banner">
+				<h1>smwhacking.de</h1>
+				<p>die deutschsprachige Seite übers SMW-Hacken</p>
+			</div>
 			<nav>
 				<ul>
-					<li>Start</li>
-					<li>Was ist SMW-Hacken?</li>
-					<li>Forum</li>
-					<li>Chat</li>
-					<li>Uploader</li>
+					<li><a href="#">Start</a></li>
+					<li><a href="#">Was ist SMW-Hacken?</a></li>
+					<li><a href="#">Forum</a></li>
+					<li><a href="#">Chat</a></li>
+					<li><a href="#">Uploader</a></li>
 				</ul>
 			</nav>
-			<div>
-				Einloggen
-			</div>
+			<nav class="user-menu">
+				<ul>
+					<li><a href="#">Einloggen</a></li>
+				</ul>
+			</nav>
 		</header>
 
-		<div>
-			Willkommen!
+		<div class="main">
+			<h2>Willkommen!</h2>
+			<p><em>smwhacking.de</em> ist eine der wenigen deutschsprachigen Seiten übers SMW-Hacken. Es geht darum, das
+				SNES-Spiel <em>Super Mario World</em> zu bearbeiten und eigene Level &mdash; oder gleich sein ganz
+				eigenes Spiel &mdash; zu bauen.<br />
+				Auf dieser Seite findest du Hacks, die andere Leute erstellt haben, Infos und Tutorials rund ums Hacken,
+				und Ressourcen, die du in deinen eigenen Hacks benutzen kannst (Grafiken, Blöcke, Musik und einiges
+				mehr). Wir haben auch ein <a href="/forum">Forum</a>, in dem du mitdiskutieren und Fragen zum Hacken
+				stellen kannst!</p>
+			<p>Ich bin <em>WYE</em> (oder auch <em>WhiteYoshiEgg</em>, oder auch <em>vvye</em>, oder auch
+				<em>mebamme</em>). Im Jahr 2008 habe ich das Forum gegründet, etwas später kam dann diese Seite hier
+				dazu. Danke auch an <em>Suyo</em>, der das damalige Backend programmiert hat, und an <em>RPG Hacker</em>,
+				der sich um das Hosting kümmert!</p>
+			<p>Wir sollten nicht verschweigen, dass die deutsche SMW-Hacking-Gemeinde ihre besten Zeiten hinter sich
+				hat &mdash; das Forum ist nicht mehr besonders aktiv, und auf dieser Seite lädt fast niemand mehr neue
+				Hacking-Materialien mehr hoch. Die zentrale Anlaufstelle für das SMW-Hacken ist nach wie vor die
+				englischsprachige Seite <a href="http://www.smwcentral.net">SMW Central</a>, auf der seit 2006
+				SMW-Hacker aus aller Welt aktiv sind. Dort werden fast alle Hacks, Ressourcen und Tutorials
+				veröffentlicht, und davon wirst du sehr viel mehr finden als hier. smwhacking.de ist nur ein kleines
+				Licht &mdash; wenn du deinen eigenen guten Hack bauen und bekannt machen willst, kommst du an SMW
+				Central nicht vorbei.</p>
+			<p>Trotzdem hoffen wir, dass du dich auf dieser Seite umschaust, ein paar nützliche Sachen findest &mdash;
+				und auch dem Forum einen Besuch abstattest. :)<br />
+				Viel Spaß beim Hacken!</p>
 		</div>
 
 	</body>
