12239e34334868e1abfe708333c4fce3cb32f621
vvye <ekaiser.hellwege@gmail.com>
2016-06-11 15:49:16 +0200

    designed reply page

---
+++ inc/pages/new-reply.php
@@ -0,0 +1,8 @@
+<h2>Beitrag schreiben</h2>
+
+<form>
+
+	<textarea name="content" placeholder="Beitrags-Text"></textarea>
+	<button class="primary" type="submit" name="submit">Beitrag absenden</button>
+
+</form>
