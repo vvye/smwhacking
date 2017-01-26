e7c437ffc2ceb3486392164321599f9be74b6133
vvye <ekaiser.hellwege@gmail.com>
2017-01-26 22:41:27 +0100

    fixed registration email sending & encoding

--- inc/functions/register.php
+++ inc/functions/register.php
@@ -103,7 +103,11 @@
 			echo $messageBody;
 		}
 
-		mail($email, MSG_REGISTRATION_EMAIL_SUBJECT, $messageBody);
+		$headers = 'From: ' . NOTIFICATION_SENDER_ADDRESS . "\r\n";
+		$headers .= 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+
+		mail($email, MSG_REGISTRATION_EMAIL_SUBJECT, $messageBody, $headers);
 	}
 	
 	
