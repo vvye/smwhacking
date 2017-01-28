a23d4f8a47a6559733c4416fb414cd6febbf9acb
vvye <ekaiser.hellwege@gmail.com>
2017-01-28 19:13:07 +0100

    added related pages to menu

--- inc/config/engine.php
+++ inc/config/engine.php
@@ -14,15 +14,20 @@
 		], [
 			'page'         => 'forums',
 			'caption'      => CAPTION_FORUM,
-			'relatedPages' => ['forum', 'thread', 'new-post', 'edit-post', 'delete-post', 'moderate-thread', 'user']
+			'relatedPages' => ['forum', 'thread', 'new-post', 'edit-post', 'delete-post', 'moderate-thread',
+				'admin', 'award-automatic-medals', 'award-medal', 'ban', 'medals', 'move-thread', 'new-pm', 'new-post',
+				'new-thread', 'pm', 'posts', 'watch-thread'
+			]
 		], [
-			'page'    => 'users',
-			'caption' => CAPTION_USERS
+			'page'         => 'users',
+			'caption'      => CAPTION_USERS,
+			'relatedPages' => ['user', 'edit-profile']
 		], [
 			'page'    => 'chat',
 			'caption' => CAPTION_CHAT
 		], [
-			'page'    => 'files',
-			'caption' => CAPTION_FILES
+			'page'         => 'files',
+			'caption'      => CAPTION_FILES,
+			'relatedPages' => ['upload']
 		],
 	]);
