04fbdfd2d03bbdd87d395a805b5dc977b3efaf8a
vvye <ekaiser.hellwege@gmail.com>
2017-01-28 19:57:26 +0100

    added post toolbar to "new thread" page

--- inc/lang/de/tmpl/new_thread.php
+++ inc/lang/de/tmpl/new_thread.php
@@ -7,6 +7,14 @@
 
 <form method="post" action="?p=new-thread&forum=<?= $forumId ?>">
 	<input type="text" class="thread-title" name="thread-title" placeholder="Titel" value="<?=  $threadTitle ?>" />
-	<textarea class="post-text" name="post-text" placeholder="Beitrags-Text"><?=  $postText ?></textarea>
+	<textarea class="post-text" name="post-text" id="post-text" placeholder="Beitrags-Text"><?= $postText ?></textarea>
 	<button class="primary" type="submit" name="submit">Thema erstellen</button>
 </form>
+
+<script type="text/javascript" src="js/cuteedit.js"></script>
+<script type="text/javascript" src="js/smiley_editor.js.php"></script>
+<script type="text/javascript">
+
+    var editor = new CuteEdit('post-text');
+
+</script>
