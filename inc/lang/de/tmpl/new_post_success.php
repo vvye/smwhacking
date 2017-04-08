b686651e62ab6aeea2e5f8ba9ae9dd9cb95b126c
vvye <ekaiser.hellwege@gmail.com>
2017-04-08 12:04:39 +0200

    added javascript redirection after new posts&threads

--- inc/lang/de/tmpl/new_post_success.php
+++ inc/lang/de/tmpl/new_post_success.php
@@ -1,2 +1,7 @@
+Du wirst automatisch weitergeleitet&hellip;<br />
 <a href="?p=thread&id=<?= $threadId ?>&page=<?= $page ?>#post-<?= $postId ?>">
-	<i class="fa fa-arrow-right"></i> Den Beitrag ansehen</a>
+	<i class="fa fa-arrow-right"></i> Den Beitrag sofort ansehen</a>
+
+<script type="text/javascript">
+    window.location = '?p=thread&id=<?= $threadId ?>&page=<?= $page ?>#post-<?= $postId ?>';
+</script>
--- inc/lang/de/tmpl/new_thread_success.php
+++ inc/lang/de/tmpl/new_thread_success.php
@@ -1,1 +1,6 @@
-<a href="?p=thread&id=<?= $threadId ?>"><i class="fa fa-arrow-right"></i> Thema ansehen</a>
+Du wirst automatisch weitergeleitet&hellip;<br />
+<a href="?p=thread&id=<?= $threadId ?>"><i class="fa fa-arrow-right"></i> Thema sofort ansehen</a>
+
+<script type="text/javascript">
+    window.location = '?p=thread&id=<?= $threadId ?>';
+</script>
